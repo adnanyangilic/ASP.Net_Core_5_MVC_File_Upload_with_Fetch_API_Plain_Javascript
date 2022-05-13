162777326612a5c4f04c558752ad59ff512014cc
adnanyangilic <adnan.alistika@gmail.com>
2022-05-13 12:20:34 +0300

    Proje dosyası ekle.

---
+++ Francisco5/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Francisco5.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Francisco5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment env;
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment e)
+        {
+            _logger = logger;
+            env = e;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Fileexpert(IFormFile pic)
+        {
+            
+            if (pic!=null) 
+            {
+                var filePath = Path.Combine(env.WebRootPath, Path.GetFileName(pic.FileName));
+                pic.CopyTo(new FileStream(filePath, FileMode.Create));
+                
+            }
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
